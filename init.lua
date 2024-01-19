a136147b1437d12ac1638c3b6549de3d004ec489
Ana M. Neri <neriam@mit.edu>
2024-01-19 14:30:27 -0500

    autoformat init.lua

--- init.lua
+++ init.lua
@@ -233,9 +233,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
-vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
-vim.api.nvim_set_hl(0, "ColorColumn", {link = "CursorLine"})
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "ColorColumn", { link = "CursorLine" })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
